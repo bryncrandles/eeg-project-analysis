44a5ba513c2331075305757283f037162187ab99
Bryn Crandles <64491479+bryncrandles@users.noreply.github.com>
2021-06-25 11:02:51 -0400

    function to test PSD

--- tests/test_psd_fct.m
+++ tests/test_psd_fct.m
@@ -1,4 +1,4 @@
-function psd = test_psd(f, fs, breakpoints, window_length, overlap, window)
+function psd = test_psd_fct(f, fs, breakpoints, window_length, overlap, window)
 
 % test PSD using sine wave
 
@@ -9,17 +9,15 @@ function psd = test_psd(f, fs, breakpoints, window_length, overlap, window)
 %   breakpoints (vec): 0 must be first entry. 2*fs must be last entry.
 %   window_length (int): length of windows
 %   overlap (int [1, fs/2]): amount of overlap for calculating power spectrum. For no overlap, use 1.
-%   window (logical): 1- true- use hann window function 
+%   window (logical): 1 (true) use hann window function 
 
 
 % Outputs
 %   psd (vec): vector of power spectrum for test sine wave.
 
-%f = 8;  % 8 hz
-%fs = 250; % sampling rate
-%epsilon = normrnd(0, 1/10, 1,  2*fs); % normal error
+
 t = 0:(2 * fs - 1); % time 
-x = sin(2 * pi * f/fs  * t); % + epsilon % generate 5 Hz sine wave
+x = sin(2 * pi * f/fs  * t); % generate sine wave 
 
 % compute PSD 
 psd = PSD(x, window_length, breakpoints, overlap, window);
